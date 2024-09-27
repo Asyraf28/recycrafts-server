0d828afa4e520249ea988179792c7698074d7fe8
Asyraf28 <mohasyraf28@gmail.com>
2024-09-28 02:57:13 +0700

    Konfigurasi host and port

--- config/Database.js
+++ config/Database.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     dialectModule: mysql2, 
 });
