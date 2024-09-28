f8e23456744057973edc7dd06b13bdd372f91c9c
Asyraf28 <mohasyraf28@gmail.com>
2024-09-28 11:51:12 +0700

    Fixed Error Vercel #2

--- config/Database.js
+++ config/Database.js
@@ -3,6 +3,7 @@ import mysql2 from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
+await sequelize.sync({ force: false });
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
